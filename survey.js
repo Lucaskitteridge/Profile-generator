295a79b6576d6cc0eb4b8106ed4d7fbe96a7c1d4
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-12-01 23:12:40 +0000

    linted

--- survey.js
+++ survey.js
@@ -13,11 +13,11 @@ rl.question("What's your name? Nicknames are also acceptable :)", (answer1) => {
           rl.question("Which sport is your absolute favourite?", (answer6) => {
             rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer7) => {
               console.log(`Hi! My name is ${answer1}. I like to listen to ${answer3} while ${answer2}.\nMy favourite meal of the day is ${answer4} and i espesically love ${answer5}.\nAs for sports, I really like ${answer6}. I'm also really good at ${answer7}`);
-  rl.close();
-});
-});
-});
-});
-});
-});
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
